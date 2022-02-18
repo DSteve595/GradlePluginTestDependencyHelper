54492c08ffcaf02c4301d4da6a977479486b8768
Steven Schoen <steven.schoen@reddit.com>
2022-02-18 13:55:01 -0500

    Rearrange project, publish test-sources-api jar

--- plugin/test-sources-api/src/main/java/com/stevenschoen/gradle/plugintestdependencyhelper/PluginTestDependencyHelper.java
+++
@@ -1,6 +0,0 @@
-package com.stevenschoen.gradle.plugintestdependencyhelper;
-
-public class PluginTestDependencyHelper {
-    public static final String repository = "STUB"; // Replaced during build
-    public static final String pluginVersion = "STUB"; // Replaced during build
-}
---
+++ test-sources-api/src/main/java/com/stevenschoen/gradle/plugintestdependencyhelper/PluginTestDependencyHelper.java
@@ -0,0 +1,12 @@
+package com.stevenschoen.gradle.plugintestdependencyhelper;
+
+public class PluginTestDependencyHelper {
+    /**
+     * Will be replaced during build
+     */
+    public static final String repository = "STUB";
+    /**
+     * Will be replaced during build
+     */
+    public static final String pluginVersion = "STUB";
+}
